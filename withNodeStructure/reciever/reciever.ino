258f3b25b75732820649bca93b8deb83f7f37e97
Akash R Prabhu <akashprabhu41@gmail.com>
2023-06-07 12:51:09 +0530

    multinode completed

---
+++ withNodeStructure/reciever/reciever.ino
@@ -0,0 +1,70 @@
+#include <esp_now.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+    int id;
+    float cm;
+    float inch;
+} struct_message;
+const char* ssid = "akash";    // Your Wi-Fi network name (SSID)
+const char* password = "hehehehe"; // Your Wi-Fi network password
+const int buzzerPin = 15; // Replace with the pin number connected to your LED
+int buzzerState = LOW;
+
+const char* serverAddress = "http://192.168.43.201:8001/data"; // Server address with the data endpoint
+
+// Create a struct_message called myData
+struct_message myData;
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("Device id: ");
+  Serial.println(myData.id);
+  Serial.print("cm: ");
+  Serial.println(myData.cm);
+  Serial.print("inch: ");
+  Serial.println(myData.inch);
+  Serial.println();
+
+ if (myData.cm > 14.0) {
+  Serial.print("Device id " + String(myData.id) + " has Low volume");
+  digitalWrite(15, HIGH);
+  delay(1000*5);                       // wait for a second
+  digitalWrite(15, LOW);    // turn the LED off by making the voltage LOW
+}
+else{
+  digitalWrite(15, LOW);
+}
+
+  
+}
+ 
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+   pinMode(15, OUTPUT);
+  
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  Serial.println(WiFi.macAddress());
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+
+}
---
+++ withNodeStructure/sender/sender.ino
@@ -0,0 +1,101 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+// REPLACE WITH YOUR RECEIVER MAC Address
+uint8_t broadcastAddress[] = {0x08,0xB6,0x1F,0x3B,0x06,0x4C};
+
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
+  char id;
+  float cm;
+  float inch;
+} struct_message;
+
+const int trigPin = 5;
+const int echoPin = 18;
+
+//define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
+
+// Create a struct_message called myData
+struct_message myData;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+ 
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED/2;
+  
+  // Convert to inches
+  distanceInch = distanceCm * CM_TO_INCH;
+  
+  // Set values to send
+  myData.id=2;
+  myData.cm=distanceCm;
+  myData.inch=distanceInch;
+  
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(2000);
+}
